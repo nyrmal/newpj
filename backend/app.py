ab271a26f899ccc72a1b9be0c51c220253a56000
nyrmal <taan55374@gmail.com>
2020-12-18 22:14:31 +0700

    update get all

--- backend/app.py
+++ backend/app.py
@@ -67,14 +67,15 @@ def get_user_by_id(customer_id):
 @app.route('/user/update/<int:customer_id>', methods=['PUT'])
 def update_user_by_id(customer_id):
     data = request.json
-    c = bo.Customer(CustomerID=data['CustomerID'], CustomerName=data['CustomerName'], ContactName=data['ContactName'], Address=data['Address'], City=data['City'], PostalCode=data['PostalCode'], Country=data['Country'])
+    c = bo.Customer(CustomerID=customer_id, CustomerName=data['CustomerName'], ContactName=data['ContactName'], Address=data['Address'], City=data['City'], PostalCode=data['PostalCode'], Country=data['Country'])
     result = do.Customer(ConnectionData).update(c)
     return jsonify({'message': result[0]}), result[1]
 
-@app.route('/user/update/<int:customer_id>', methods=['DELETE'])
+@app.route('/user/delete/<int:customer_id>', methods=['DELETE'])
 def delete_user_by_id(customer_id):
     c = bo.Customer(CustomerID=customer_id)
     result = do.Customer(ConnectionData).delete(c)
     return jsonify({'message': result[0]}), result[1]
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
